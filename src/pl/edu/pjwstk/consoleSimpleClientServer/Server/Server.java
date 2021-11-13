d3aeaa5da003613f27f3f93c29d852407b9ae4cc
pawel.d.dabrowski@gmail.com <pawel.d.dabrowski@gmail.com>
2021-11-13 22:30:27 +0100

    Start coding server.

---
+++ src/pl/edu/pjwstk/consoleSimpleClientServer/Server/Server.java
@@ -0,0 +1,25 @@
+package pl.edu.pjwstk.consoleSimpleClientServer.Server;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) {
+
+        Socket socket = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        ServerSocket serverSocket = null;
+
+        try {
+
+        }
+    }
+}
